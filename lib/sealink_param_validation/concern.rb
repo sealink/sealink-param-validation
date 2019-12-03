e6c8ff0a68f9c76e1248c0f457341730d9f1fd27
Sean Earle <sean.r.earle@gmail.com>
2019-12-03 12:07:20 +1030

    [TT-6430] Set up the gem and specs

---
+++ lib/sealink_param_validation/concern.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'active_support/concern'
+
+module SealinkParamValidation
+  module Concern
+    extend ActiveSupport::Concern
+
+    included do
+      # Accessor for validation results hash
+      attr_reader :validation_result
+      class_attribute :schemas
+      self.schemas = {}
+
+      def self.schema_for(action, schema = nil)
+        schemas[action.to_sym] = schema if schema
+        before_action :ensure_schema
+      end
+    end
+
+    private
+
+    def ensure_schema!
+      @validation_result = schema_for_action.call(params.to_unsafe_h)
+      return if @validation_result.success?
+      error = SealinkParamValidation::ValidationHelper.generate_humanized_error_message(@validation_result)
+      fail SealinkParamValidation::InvalidInputError, error
+    end
+
+    def ensure_schema
+      schema = schema_for_action
+      return unless schema.present?
+      @validation_result = schema.call(params.to_unsafe_h)
+      return if @validation_result.success?
+      render json: {error: SealinkParamValidation::ValidationHelper.generate_error_message(@validation_result)}, status: 422
+    end
+
+    def action
+      params['action'].to_sym
+    end
+
+    def schema_for_action
+      self.class.schemas[action]
+    end
+  end
+end
